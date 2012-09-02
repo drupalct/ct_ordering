4ed606aa0759100d0fe461913387bc33da6b04e5
Boy <andong135@126.com>
2012-09-02 21:58:13 +0800

    By Boy: Implements shopping cart update and submit

--- sites/all/modules/ct_ordering/ct_ordering_cart/ct_ordering_cart.js
+++ sites/all/modules/ct_ordering/ct_ordering_cart/ct_ordering_cart.js
@@ -32,21 +32,51 @@
         jsonObj[jsonObj.length] = new Object();
         jsonObj[jsonObj.length-1].FoodID = id;
         jsonObj[jsonObj.length-1].Name = name;
-        jsonObj[jsonObj.length-1].Num = num;
+        jsonObj[jsonObj.length-1].Num = _num;
         jsonObj[jsonObj.length-1].Price = price;
       }
       $.cookie('cookieFoodID', JSON.stringify(jsonObj), cookieSet); //需要json2.js支持
     }
   }
+  
+  function RefreshShoppingCart(id, name, num, price) {
+    var cookieSet = { expires: 7, path: '/'};
+    var jsonObj = eval('(' + $.cookie('cookieFoodID') + ')'); //将json字符串转换成数组
+    var findFood = false;
+    for (var food in jsonObj) {
+      if (jsonObj[food].FoodID == id) {
+        jsonObj[food].Name = name;
+        jsonObj[food].Num = num;
+        jsonObj[food].Price = price;
+      }
+    $.cookie('cookieFoodID', JSON.stringify(jsonObj), cookieSet); //需要json2.js支持
+    }
+  }
+  
+  function LoadFoods() {
+    if ($.cookie('cookieFoodID') != null) {
+      var jsonObj = eval('(' + $.cookie('cookieFoodID') + ')'); //将json字符串转换成数组
+      var $foods = '';
+      for (var food in jsonObj) {
+        $foods += '<div class="food-item"><span class="food-name" fid="' + jsonObj[food].FoodID + '" price="' + jsonObj[food].Price + '">' + jsonObj[food].Name + '</span>';
+        $foods += '<span class="num-minus">-</span><span class="food-order">' + jsonObj[food].Num + '</span><span class="num-plus">+</span>';
+        $foods += '<span class="food-price">' + jsonObj[food].Price * jsonObj[food].Num + '</span></div>';
+      }
+      $('#block-ct-ordering-cart-info .content').html($foods);
+    }
+  }
+  
   $(function () {
-    $('.num-plus').click(function () {
+    LoadFoods();
+    
+    $('.num-plus').live('click', function () {
       $food_order = $(this).siblings('.food-order');
       $num = $food_order.html();
       $num = num_add($num);
       $food_order.html($num);
     });
     
-    $('.num-minus').click(function () {
+    $('.num-minus').live('click', function () {
       $food_order = $(this).siblings('.food-order');
       $num = $food_order.html();
       if ($num > 1) {
@@ -58,25 +88,30 @@
       }
     });
     
+    $('.food-item .num-plus, .food-item .num-minus').live('click', function () {
+      $num = $(this).siblings('.food-order').html();
+      $id = $(this).siblings('.food-name').attr('fid');
+      $name = $(this).siblings('.food-name').html();
+      $price = $(this).siblings('.food-name').attr('price');
+      RefreshShoppingCart($id, $name, $num, $price);
+      $(this).siblings('.food-price').html($price * $num);
+    });
+    
     $('.add-to-cart').click(function () {
       $food_info = $(this).siblings().children('.food-order');
       $food_check = $food_info.parent();
-      //$('#block-ct-ordering-cart-info .content').append($food_check.clone(), $food_info.attr('name'), $food_info.attr('price')*$food_info.html());
       var fid = $food_info.attr('fid');
       var fname = $food_info.attr('name');
       var fprice = $food_info.attr('price');
       var fnum = $food_info.html();
       AddToShoppingCart(fid, fname, fnum, fprice);
       
-      var jsonObj = eval('(' + $.cookie('cookieFoodID') + ')'); //将json字符串转换成数组
-      var $foods = '';
-      for (var food in jsonObj) {
-        //alert(jsonObj[food].Price);
-        $foods += jsonObj[food].Name;
-        $foods += jsonObj[food].Num;
-        $foods += jsonObj[food].Price * jsonObj[food].Num;
-      }
-      $('#block-ct-ordering-cart-info .content').html($foods);
+      LoadFoods();
+    });
+    
+    $('#ct-restaurant-order-form .form-submit').live('click', function () {
+      $('#content').append($.cookie('cookieFoodID'));
+      $.cookie('cookieFoodID', null, { path: '/' });
     });
   });
 })(jQuery, Drupal, this, this.document);
