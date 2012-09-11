440e29e9e3de7e540e5cce975f032336bb478ed4
Boy <andong135@126.com>
2012-09-11 21:55:59 +0800

    By Boy: Change cookie name for backend process

--- sites/all/modules/ct_ordering/ct_ordering_cart/ct_ordering_cart.js
+++ sites/all/modules/ct_ordering/ct_ordering_cart/ct_ordering_cart.js
@@ -16,11 +16,11 @@
     }
     var cookieSet = { expires: 7, path: '/'};
     var jsonStr = "[{'FoodID':'" + id + "','Name':'" + name + "','Num':'" + _num + "','Price':'" + price + "'}]";
-    if ($.cookie('cookieFoodID') == null) {
-      $.cookie('cookieFoodID', jsonStr, cookieSet);
+    if ($.cookie('order_info') == null) {
+      $.cookie('order_info', jsonStr, cookieSet);
     }
     else {
-      var jsonObj = eval('(' + $.cookie('cookieFoodID') + ')'); //将json字符串转换成数组
+      var jsonObj = eval('(' + $.cookie('order_info') + ')'); //将json字符串转换成数组
       var findFood = false;
       for (var food in jsonObj) {
         if (jsonObj[food].FoodID == id) {
@@ -35,37 +35,37 @@
         jsonObj[jsonObj.length-1].Num = _num;
         jsonObj[jsonObj.length-1].Price = price;
       }
-      $.cookie('cookieFoodID', JSON.stringify(jsonObj), cookieSet); //需要json2.js支持
+      $.cookie('order_info', JSON.stringify(jsonObj), cookieSet); //需要json2.js支持
     }
   }
   
   function RefreshShoppingCart(id, name, num, price) {
     var cookieSet = { expires: 7, path: '/'};
-    var jsonObj = eval('(' + $.cookie('cookieFoodID') + ')'); //将json字符串转换成数组
+    var jsonObj = eval('(' + $.cookie('order_info') + ')'); //将json字符串转换成数组
     for (var food in jsonObj) {
       if (jsonObj[food].FoodID == id) {
         jsonObj[food].Name = name;
         jsonObj[food].Num = num;
         jsonObj[food].Price = price;
       }
-    $.cookie('cookieFoodID', JSON.stringify(jsonObj), cookieSet); //需要json2.js支持
+    $.cookie('order_info', JSON.stringify(jsonObj), cookieSet); //需要json2.js支持
     }
   }
   
   function RemoveFood(id) {
     var cookieSet = { expires: 7, path: '/'};
-    var jsonObj = eval('(' + $.cookie('cookieFoodID') + ')'); //将json字符串转换成数组
+    var jsonObj = eval('(' + $.cookie('order_info') + ')'); //将json字符串转换成数组
     for (var food in jsonObj) {
       if (jsonObj[food].FoodID == id) {
         jsonObj.splice(food);
       }
-    $.cookie('cookieFoodID', JSON.stringify(jsonObj), cookieSet); //需要json2.js支持
+    $.cookie('order_info', JSON.stringify(jsonObj), cookieSet); //需要json2.js支持
     }
   }
   
   function LoadFoods() {
-    if ($.cookie('cookieFoodID') != null) {
-      var jsonObj = eval('(' + $.cookie('cookieFoodID') + ')'); //将json字符串转换成数组
+    if ($.cookie('order_info') != null) {
+      var jsonObj = eval('(' + $.cookie('order_info') + ')'); //将json字符串转换成数组
       var $foods = '';
       for (var food in jsonObj) {
         $foods += '<div class="food-item"><span class="food-name" fid="' + jsonObj[food].FoodID + '" price="' + jsonObj[food].Price + '">' + jsonObj[food].Name + '</span>';
@@ -126,9 +126,5 @@
       LoadFoods();
     });
     
-    $('#ct-restaurant-order-form .form-submit').live('click', function () {
-      $('#content').append($.cookie('cookieFoodID'));
-      $.cookie('cookieFoodID', null, { path: '/' });
-    });
   });
 })(jQuery, Drupal, this, this.document);
